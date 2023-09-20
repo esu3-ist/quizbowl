4d327270ebf4d96797fb7423928f004b2f50dc89
Brian Collett <bcollett@esu3.org>
2023-09-20 09:33:06 -0500

    Changed text when time is up per question.

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import './App.css';
 
 const renderTime = ({ remainingTime }) => {
   if (remainingTime === 0) {
-    return <div className="timer">Too late...</div>;
+    return <div className="timer">Times Up!</div>;
   }
 
   return (
