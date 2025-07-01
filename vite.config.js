1247669693b55dc7e24688b469e0e58556970502
Collett <bcollett@esu3.org>
2025-07-01 14:58:05 -0500

    Fix build to see /quizbowl/ as root.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isGitHub = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig(() => {
   return {
+    base: isGitHub ? '/quizbowl/' : '/',
     build: {
       outDir: 'build',
     },
